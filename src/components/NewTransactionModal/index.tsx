04fc61dc34f57b98542b4210d3ee01a1cb1018d7
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-02-09 09:32:15 -0300

    Adding styles to Modal Buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
-import Modal from "react-modal";
-import { Container } from "./styles";
+import Modal from "react-modal"
+import { Container, TransactionTypeContainer } from "./styles"
 
-import closeImg from "../../assets/close.svg";
+import closeImg from "../../assets/close.svg"
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg"
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -31,6 +33,16 @@ export function NewTransactionModal({
 
         <input placeholder="Título" />
         <input type="number" placeholder="Valor" />
+        <TransactionTypeContainer>
+          <button type="button" >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button" >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
